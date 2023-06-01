5f4ff5583b04b37681878948c5ba062a9d8178a3
Jorge Casado <130782016+jorgecasadob@users.noreply.github.com>
2023-06-01 01:18:11 +0000

    Prueba de excuse generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,24 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const element = document.querySelector("#excuse");
+  element.innerText = randomExcuse();
 };
+
+function randomExcuse() {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying",
+  ];
+
+  return `${who[Math.floor(Math.random() * who.lenght)]} 
+${action[Math.floor(Math.random() * action.lenght)]} 
+${what[Math.floor(Math.random() * what.lenght)]} 
+${when[Math.floor(Math.random() * when.lenght)]}`;
+}
